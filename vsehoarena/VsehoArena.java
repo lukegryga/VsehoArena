7ff0cedc0ac1a9e731a728e6a2d978f479a018ca
Tomáš Zelina <zelitomas@gmail.com>
2017-01-11 20:42:23 +0100

    Ignore letter cases in commands

--- vsehoarena/VsehoArena.java
+++ vsehoarena/VsehoArena.java
@@ -61,30 +61,26 @@ public class VsehoArena extends JavaPlugin {
                 break;
             
             case 2:
-                if(args[0].equals("create")){
+                if(args[0].equalsIgnoreCase("create")){
                     aBuilder.createArena(args[1], pSender);
-                }else if(args[0].equals("addstart")){
+                }else if(args[0].equalsIgnoreCase("addstart")){
                     aBuilder.registerChest(pSender, args[1], true);
                 }
-                else if(args[0].equals("addwild")){
+                else if(args[0].equalsIgnoreCase("addwild")){
                     aBuilder.registerChest(pSender, args[1], false);
-                }else if(args[0].equals("start")){
+                }else if(args[0].equalsIgnoreCase("start")){
                     Arena a = aBuilder.getArena(args[1], pSender);
                     if(a != null)
                         a.startGame();
-                }else if(args[0].equals("regen")){
+                }else if(args[0].equalsIgnoreCase("regen")){
                     Arena a = aBuilder.getArena(args[1], pSender);
                     if(a != null)
                         a.regenerateArena();
-                }else if(args[0].equals("startitems")){
+                }else if(args[0].equalsIgnoreCase("startitems")){
                     Arena a = aBuilder.getArena(args[1], pSender);
                     if(a != null)
                         a.showStartInv(pSender);
-                }else if(args[0].equals("addwildinv")){
-                    Arena a = aBuilder.getArena(args[1], pSender);
-                    if(a != null)
-                        a.createNewWildInv();
-                }else if(args[0].equals("wilditems")){
+                }else if(args[0].equalsIgnoreCase("wilditems")){
                     Arena a = aBuilder.getArena(args[1], pSender);
                     if(a != null)
                         a.showWildInv(1, pSender);
@@ -92,7 +88,7 @@ public class VsehoArena extends JavaPlugin {
                 break;
                 
             case 3:
-                if(args[0].equals("wilditems")){
+                if(args[0].equalsIgnoreCase("wilditems")){
                     try{
                     Integer number = Integer.valueOf(args[1]);
                     Arena a = aBuilder.getArena(args[2], pSender);
